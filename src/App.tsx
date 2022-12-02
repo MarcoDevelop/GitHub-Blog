304610d3d3c4a83df91d0eaaf1a3bb78dd6d6ea4
MarcoDevelop <m.miranda2015@outlook.com>
2022-12-02 18:10:16 -0300

    Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 import { Router } from './Routers'
+import { defaultTheme } from './styles/theme/default'
 
 export function App() {
   return (
     <BrowserRouter>
-      <Router />
+      <ThemeProvider theme={defaultTheme}>
+        <Router />
+      </ThemeProvider>
     </BrowserRouter>
   )
 }
