8cfcbd9bd819b109adfa37bdec32a436202d8e0e
MarcoDevelop <m.miranda2015@outlook.com>
2022-12-03 16:12:23 -0300

    icons

--- src/components/LinkProfile/index.tsx
+++ src/components/LinkProfile/index.tsx
@@ -1,9 +1,17 @@
+import { faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { ComponentProps, ComponentType } from 'react'
 import { LinkProfileContainer } from './styles'
 
-interface LinkProfileProps {
+type LinkProfileProps = ComponentProps<typeof LinkProfileContainer> & {
   text: string
 }
 
-export function LinkProfile({ text }: LinkProfileProps) {
-  return <LinkProfileContainer>{text}</LinkProfileContainer>
+export function LinkProfile({ text, ...rest }: LinkProfileProps) {
+  return (
+    <LinkProfileContainer {...rest}>
+      {text}
+      <FontAwesomeIcon icon={faUpRightFromSquare} />
+    </LinkProfileContainer>
+  )
 }
--- src/components/LinkProfile/styles.ts
+++ src/components/LinkProfile/styles.ts
@@ -20,5 +20,6 @@ export const LinkProfileContainer = styled.a`
   }
 
   &:hover {
+    border-color: ${({ theme }) => theme.colors['brand-blue']};
   }
 `
