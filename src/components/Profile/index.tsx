042ed11f36f04faa6d46b36934356021373b914d
MarcoDevelop <m.miranda2015@outlook.com>
2022-12-03 16:12:16 -0300

    Icons FontAwesomeIcon

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { LinkProfile } from '../LinkProfile'
 import { ProfileContainer, ProfileDetails, ProfileImage } from './styles'
 
 export function Profile() {
@@ -7,7 +9,21 @@ export function Profile() {
       <ProfileDetails>
         <header>
           <h1>Marco Miranda</h1>
+
+          <LinkProfile text="GitHub" href="#" />
         </header>
+        <p>
+          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
+          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
+          pulvinar vel mass.
+        </p>
+        <ul>
+          <li>
+            <FontAwesomeIcon icon={} />
+          </li>
+          <li></li>
+          <li></li>
+        </ul>
       </ProfileDetails>
     </ProfileContainer>
   )
