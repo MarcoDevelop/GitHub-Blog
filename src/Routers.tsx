2cf1b326dc5ac5431a347b001201b8c184ab178e
MarcoDevelop <m.miranda2015@outlook.com>
2022-12-02 21:35:07 -0300

    ROTAS

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
+import { DefaultLayout } from './DefaultLayout'
 import { Home } from './Pages/Home'
 
 export function Router() {
   return (
     <Routes>
+      <Route path="/" element={<DefaultLayout />} />
       <Route path="/" element={<Home />} />
     </Routes>
   )
