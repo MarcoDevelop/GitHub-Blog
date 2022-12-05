427b0834402299a0331b2409fe979c3fd39edeaf
MarcoDevelop <m.miranda2015@outlook.com>
2022-12-05 17:22:00 -0300

    Post home

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,11 +1,19 @@
+import { Post } from '../../components/Post'
 import { Profile } from '../../components/Profile'
 import { SearchInput } from '../../components/SearchInput'
+import { PostsListContainer } from './styles'
 
 export function Home() {
   return (
     <>
       <Profile />
       <SearchInput />
+      <PostsListContainer>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </PostsListContainer>
     </>
   )
 }
